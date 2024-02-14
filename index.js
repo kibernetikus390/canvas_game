2c4f8447cd644461ca5a7fafce23d9918a57054e
kibernetikus390 <naturaspira@gmail.com>
2024-02-14 12:55:52 +0000

    0102

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+const x = canvas.width/2;
+const y = canvas.height/2;
 const c = canvas.getContext("2d");
 
-console.log(c);
+class Player{
+    constructor(x,y,radius,color){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    
+    draw()
+    {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, 360, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+}
+
+const player = new Player(x,y,30,"blue");
+player.draw();
+
+console.log(player);
