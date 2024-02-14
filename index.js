b3407423acfa72fb5a5973553c9767df9fc50297
kibernetikus390 <naturaspira@gmail.com>
2024-02-14 13:21:58 +0000

    0103 shoot projectiles

--- index.js
+++ index.js
@@ -20,9 +20,61 @@ class Player{
         c.fillStyle = this.color;
         c.fill();
     }
-}
+};
+
+class Projectile{
+    constructor(x,y,r,c,v)
+    {
+        this.x = x;
+        this.y = y;
+        this.radius = r;
+        this.color = c;
+        this.velocity = v;
+    }
+    draw()
+    {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, 360, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+    update()
+    {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;   
+    }
+};
 
 const player = new Player(x,y,30,"blue");
 player.draw();
 
+const projectiles = [];
+
+
+function animate()
+{
+    window.requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    projectiles.forEach(p => {
+       p.update(); 
+    });
+    
+}
+
+window.addEventListener("click",(event)=>{
+    let angle = Math.atan2(event.clientY - y, event.clientX - x);
+    let velocity = {
+        x:Math.cos(angle),
+        y:Math.sin(angle)
+    };
+    projectiles.push(new Projectile(
+        canvas.width / 2,
+        canvas.height / 2,
+        5, "red", velocity));
+});
+
+
+animate();
 console.log(player);
